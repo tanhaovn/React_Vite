4018ed23dca285369f530037e9f047a95233f8ef
TanHao <justinxie2406@gmail.com>
2025-03-25 14:56:14 +0700

    learn react

---
+++ hoc-react/src/components/learn/MyComponent.jsx
@@ -0,0 +1,4 @@
+const MyComponent = () => {
+  return <div>Phạm Tấn Hào he he</div>;
+};
+export default MyComponent;
---
+++ hoc-react/src/components/learn/SecondComponent.jsx
@@ -0,0 +1,8 @@
+const ThirdComponent = () => {
+  return <div>Third components</div>;
+};
+
+const SecondComponent = () => {
+  return <div>Second components</div>;
+};
+export { SecondComponent, ThirdComponent };
