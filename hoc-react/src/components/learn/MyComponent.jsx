b86edb7d05ab1a96fcd7c3edb012f3ef9d6d8428
TanHao <justinxie2406@gmail.com>
2025-03-25 17:32:53 +0700

    js trong html

--- hoc-react/src/components/learn/MyComponent.jsx
+++ hoc-react/src/components/learn/MyComponent.jsx
@@ -1,5 +1,13 @@
 const MyComponent = () => {
-  return;
-  <div>Phạm Tấn Hào he he</div>;
+  const name = "Phạm Tấn Hào";
+  const array = [1, 2, 3];
+  return (
+    <>
+      <div>{JSON.stringify(array)}</div>
+      <div>{name} he he</div>
+      <div>Là Một Sinh Viên</div>
+      <div>{console.log("Phạm Tấn Hào")}</div>
+    </>
+  );
 };
 export default MyComponent;
