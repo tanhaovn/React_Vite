4de69f8ac03ba8e9ce669a76ebd527bac535a7ec
TanHao <justinxie2406@gmail.com>
2025-03-26 10:16:28 +0700

    thuchanhduan

--- hoc-react/src/App.jsx
+++ hoc-react/src/App.jsx
@@ -1,41 +1,19 @@
-import { useState } from "react";
+import "./components/todo/todo.css";
+import TodoData from "./components/todo/TodoData";
+import TodoNew from "./components/todo/todoNew";
 import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import MyComponent from "./components/learn/MyComponent";
-import {
-  SecondComponent,
-  ThirdComponent,
-} from "./components/learn/SecondComponent";
 
 const App = () => {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="todo-container">
+        <div className="todo-title">Todo List</div>
+        <TodoNew />
+        <TodoData />
+        <div className="todo-image">
+          <img src={reactLogo} className="logo" alt="React Logo" />
+        </div>
       </div>
-      <h1>Tấn Hào TPM12</h1>
-      <MyComponent />
-      <SecondComponent />
-      <ThirdComponent />
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 };
---
+++ hoc-react/src/components/todo/TodoNew.jsx
@@ -0,0 +1,9 @@
+const TodoNew = () => {
+  return (
+    <div className="todo-new">
+      <input type="test" />
+      <button>Add</button>
+    </div>
+  );
+};
+export default TodoNew;
