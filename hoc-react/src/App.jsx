681af2c8257b6b1e0804f45951dabf8c1b0a2bfa
TanHao <justinxie2406@gmail.com>
2025-03-27 02:03:04 +0700

    them du lieu

--- hoc-react/src/App.jsx
+++ hoc-react/src/App.jsx
@@ -10,11 +10,16 @@ const App = () => {
     address: "Khánh Hòa",
     country: "Việt Nam",
   };
+
+  const addNewTodo = (name) => {
+    alert(`call me ${name}`);
+  };
+
   return (
     <>
       <div className="todo-container">
         <div className="todo-title">Todo List</div>
-        <TodoNew />
+        <TodoNew addNewTodo={addNewTodo} />
         <TodoData name={hoc_react} age={age} data={data} />
         <div className="todo-image">
           <img src={reactLogo} className="logo" alt="React Logo" />
--- hoc-react/src/components/todo/TodoNew.jsx
+++ hoc-react/src/components/todo/TodoNew.jsx
@@ -1,8 +1,24 @@
-const TodoNew = () => {
+const TodoNew = (props) => {
+  console.log(props);
+  const { addNewTodo } = props;
+  // addNewTodo("Tấn Hào");
+  const handleClick = () => {
+    alert("click me");
+  };
+  const handleOnChange = (name) => {
+    console.log("Bạn là ai sau này", name);
+  };
   return (
     <div className="todo-new">
-      <input type="test" />
-      <button>Add</button>
+      <input
+        type="test"
+        onChange={(event) => {
+          handleOnChange(event.target.value);
+        }}
+      />
+      <button style={{ cursor: "pointer" }} onClick={handleClick}>
+        Add
+      </button>
     </div>
   );
 };
