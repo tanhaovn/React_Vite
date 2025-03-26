f09ec094b85bbbdd2da2d29796b321a1f76bcebd
TanHao <justinxie2406@gmail.com>
2025-03-26 20:09:35 +0700

    nguyen thao vy

--- hoc-react/src/App.jsx
+++ hoc-react/src/App.jsx
@@ -4,12 +4,18 @@ import TodoNew from "./components/todo/todoNew";
 import reactLogo from "./assets/react.svg";
 
 const App = () => {
+  const hoc_react = "Hào";
+  const age = 19;
+  const data = {
+    address: "Khánh Hòa",
+    country: "Việt Nam",
+  };
   return (
     <>
       <div className="todo-container">
         <div className="todo-title">Todo List</div>
         <TodoNew />
-        <TodoData />
+        <TodoData name={hoc_react} age={age} data={data} />
         <div className="todo-image">
           <img src={reactLogo} className="logo" alt="React Logo" />
         </div>
