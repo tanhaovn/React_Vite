33833535d4e6d3235f77522c4f1757bdf89bf316
TanHao <justinxie2406@gmail.com>
2025-03-27 22:40:26 +0700

    video 39

--- hoc-react/src/components/todo/TodoData.jsx
+++ hoc-react/src/components/todo/TodoData.jsx
@@ -1,11 +1,16 @@
 const TodoData = (props) => {
-  const { name, age, data } = props;
-  console.log("Check:", props);
+  const { todoList } = props;
+  console.log("Check:", todoList);
   return (
     <div className="todo-data">
-      <div>My name is {name}</div>
-      <div>Learning React</div>
-      <div>Watching Youtube</div>
+      {todoList.map((item, index) => {
+        return (
+          <div className="todo-item">
+            {item.name}
+            <button>Delete</button>
+          </div>
+        );
+      })}
       <div>{JSON.stringify(props.todoList)}</div>
     </div>
   );
